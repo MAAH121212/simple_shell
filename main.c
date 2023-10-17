46eb64f3d11bcd5d6e2c8c89537ac7ffa98e38ff
ahmed711 <ahmed_tayseer12@yahoo.com>
2023-10-17 21:39:23 +0200

    Shrink main file

--- main.c
+++ main.c
@@ -8,13 +8,9 @@
 
 int main(int ac, char **av, char **env)
 {
-
-    ssize_t lineSize = 0;
-    size_t len = 0;
+    ssize_t lineSize = 0; size_t len = 0;
     char *line = NULL, *path = NULL, *fullPath;
-    char **args = NULL;
-    int frk;
-    int status;
+    char **args = NULL; int frk, status;
     
     while(1)
     {   
@@ -31,48 +27,34 @@ int main(int ac, char **av, char **env)
 
         if (!(_strlen(line) - 1))
             continue;
-
         path = _strdup(_getenv("PATH"));
-
-        line[_strlen(line) - 1] = '\0';
-        args = strtow(line);
-
+        line[_strlen(line) - 1] = '\0'; args = strtow(line);
         if (get_op_func(args[0]) != NULL)
         {
-            
             if (get_op_func(args[0])() == 2)
             {
                 free_args(args); free(path); free(line);
                 exit(EXIT_SUCCESS);
             }
-            free_args(args); free(path);
-            continue;
+            free_args(args); free(path); continue;
         }
 
         fullPath = findInPATH(path, args[0]);
         if (!fullPath)
         {
             print_string("./shell: No such file or directory\n");
-            free_args(args); free(path); 
-            continue;
+            free_args(args); free(path); continue;
         }
         
-        free(args[0]);
-        args[0] = fullPath;
-        frk = fork();
+        free(args[0]); args[0] = fullPath; frk = fork();
         if (frk == 0)
         {
-            execve(args[0], args, env);
-            perror("./shell");
-            free_args(args); free(fullPath); free(path);
-            _exit(EXIT_FAILURE);
+            execve(args[0], args, env); perror("./shell");
+            free_args(args); free(fullPath); free(path); _exit(EXIT_FAILURE);
         }    
         else if (frk == -1)
-        {
             perror("./shell");
-        }
-        wait(&status);
-        free_args(args); free(path);
+        wait(&status); free_args(args); free(path);
     } 
     return (0);
 }
